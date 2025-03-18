83fac03d6e6ed8261e5ca0e137400e8880c25c9c
I.Maksat <maksat@MacBookAir.lan>
2025-03-18 12:22:21 +0600

    Added a controller class to manage users

---
+++ src/main/java/org/example/JobSearch/controller/UserController.java
@@ -0,0 +1,37 @@
+package org.example.JobSearch.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.JobSearch.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/uploadAvatar")
+    public String uploadAvatar(MultipartFile file) {
+        return userService.addAvatar(file);
+    }
+
+    @GetMapping("avatar/{imageName}")
+    public ResponseEntity<?> getImage(@PathVariable String imageName) {
+        return userService.getAvatarByName(imageName);
+    }
+
+    @GetMapping("/findApplicant/{userEmail}")
+    public ResponseEntity<String> findApplicant(@PathVariable("userEmail") Integer userEmail) {
+        userService.findApplicant(userEmail);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Applicant successfully found");
+    }
+
+    @GetMapping("/findEmployer/{userEmail}")
+    public ResponseEntity<String> findEmployer(@PathVariable("userEmail") Integer userEmail) {
+        userService.findEmployer(userEmail);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Employer successfully found");
+    }
+}
