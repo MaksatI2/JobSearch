5f1238897c73a7c898f01cc959e1dc916d1f5e54
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-03-18 15:40:36 +0600

    Added a class and interface for responding to a vacancy

---
+++ src/main/java/org/example/JobSearch/service/ApplicationService.java
@@ -0,0 +1,8 @@
+package org.example.JobSearch.service;
+
+import org.example.JobSearch.dto.RespondedApplicantDTO;
+
+public interface ApplicationService {
+
+    void respondToVacancy(Integer vacancyId, RespondedApplicantDTO respondedApplicantDto);
+}
