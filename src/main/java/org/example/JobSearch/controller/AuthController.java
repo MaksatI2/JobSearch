a3c6a5f43e5ea9cbd3e091b3fa743f13bdac7b45
I.Maksat <maksat@MacBookAir.lan>
2025-03-18 12:20:07 +0600

    Made a controller for registration

---
+++ src/main/java/org/example/JobSearch/controller/AuthController.java
@@ -0,0 +1,24 @@
+package org.example.JobSearch.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.JobSearch.dto.UserDTO;
+import org.example.JobSearch.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
+        userService.register(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+    }
+}
