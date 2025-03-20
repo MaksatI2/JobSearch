4344d3c09d495831bad86c9b2987837ddefbb9df
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-03-20 16:57:21 +0600

    I implemented all the necessary logic for searching, checking for availability. I also made a separate method for converting when outputting identical data.

--- src/main/java/org/example/JobSearch/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/JobSearch/service/impl/UserServiceImpl.java
@@ -1,25 +1,74 @@
 package org.example.JobSearch.service.impl;
 
+import org.example.JobSearch.model.User;
+import org.example.JobSearch.dao.UserDao;
 import org.example.JobSearch.util.FileUtil;
 import org.example.JobSearch.dto.UserDTO;
 import org.example.JobSearch.service.UserService;
+import org.example.JobSearch.exceptions.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
     @Override
-    public void findApplicant(Long userEmail) {
-        // TODO: сделать логику для поиска соискателя по email
+    public UserDTO findApplicant(String email) {
+        User user = userDao.findApplicant(email)
+                .orElseThrow(UserNotFoundException::new);
+        return convertToUserDTO(user);
     }
 
     @Override
-    public void findEmployer(Long userEmail) {
-        // TODO: сделать логику для поиска работодателя по email
+    public UserDTO findApplicantByPhone(String phoneNumber) {
+        User user = userDao.findApplicantByPhone(phoneNumber)
+                .orElseThrow(UserNotFoundException::new);
+        return convertToUserDTO(user);
+    }
+
+    @Override
+    public List<UserDTO> findApplicantsByName(String name) {
+        List<User> users = userDao.findApplicantsByName(name);
+        return users.stream().map(this::convertToUserDTO).toList();
+    }
+
+    @Override
+    public UserDTO findEmployer(String email) {
+        User user = userDao.findEmployer(email)
+                .orElseThrow(UserNotFoundException::new);
+        return convertToUserDTO(user);
+    }
+
+    @Override
+    public UserDTO findEmployerByPhone(String phoneNumber) {
+        User user = userDao.findEmployerByPhone(phoneNumber)
+                .orElseThrow(UserNotFoundException::new);
+        return convertToUserDTO(user);
+    }
+
+    @Override
+    public List<UserDTO> findEmployersByName(String name) {
+        List<User> users = userDao.findEmployersByName(name);
+        return users.stream().map(this::convertToUserDTO).toList();
+    }
+
+    private UserDTO convertToUserDTO(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .age(user.getAge())
+                .phoneNumber(user.getPhoneNumber())
+                .avatar(user.getAvatar())
+                .accountType(user.getAccountType())
+                .build();
     }
 
     @Override
@@ -46,5 +95,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public void getApplicantsVacancy(Long vacancyId) {
         //TODO: Сделать логику для вывода соискателей откликнувщихся на вакансию
+    }
+
+    @Override
+    public boolean userExists(String email) {
+        return userDao.existsByEmail(email);
     }
 }
