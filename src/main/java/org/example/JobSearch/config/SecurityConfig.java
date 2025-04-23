73620256c990035d8361d795133b93967c977e19
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-04-23 16:39:47 +0600

    I improved the exit button and also removed csrf

--- src/main/java/org/example/JobSearch/config/SecurityConfig.java
+++ src/main/java/org/example/JobSearch/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
                                 "/",
