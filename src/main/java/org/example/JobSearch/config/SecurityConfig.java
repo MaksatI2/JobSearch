61fb5b8e292713f1482b858015eeb1b3b5d252e8
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-04-24 17:01:02 +0600

    Made a custom check and search

--- src/main/java/org/example/JobSearch/config/SecurityConfig.java
+++ src/main/java/org/example/JobSearch/config/SecurityConfig.java
@@ -1,9 +1,10 @@
 package org.example.JobSearch.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.example.JobSearch.service.impl.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,35 +12,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final DataSource dataSource;
+    private final CustomUserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
     private final CustomSuccessHandler successHandler;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        String fetchUser = "SELECT email, password, enabled " +
-                "FROM users " +
-                "WHERE email = ?";
-
-        String fetchRoles = """
-                SELECT u.email, a.authority 
-                FROM users u 
-                JOIN authorities a ON u.account_type = a.id
-                WHERE u.email = ?
-                """;
-
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(fetchUser)
-                .authoritiesByUsernameQuery(fetchRoles)
-                .passwordEncoder(passwordEncoder);
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        return builder.build();
     }
 
     @Bean
---
+++ src/main/java/org/example/JobSearch/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package org.example.JobSearch.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.example.JobSearch.model.User;
+import org.example.JobSearch.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден: " + email));
+
+        String role = user.getAccountType().name();
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getEnabled(),
+                true,
+                true,
+                true,
+                List.of(new SimpleGrantedAuthority(role))
+        );
+    }
+}
