ee1f488cd81be6ce6efc9b40643273e661a91244
I.Maksat <maksat@MacBookAir.lan>
2025-05-19 22:01:16 +0600

    remade all templates for Bootstrap

--- src/main/java/org/example/JobSearch/service/UserService.java
+++ src/main/java/org/example/JobSearch/service/UserService.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
 public interface UserService {
 
     void  registerEmployer(EmployerRegisterDTO employerDto);
